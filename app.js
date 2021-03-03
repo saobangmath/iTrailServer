7e501515bd07f8c0e7380f526620374bc86bf48a
saobangmath <anhtai2672000@gmail.com>
2021-03-03 16:20:55 +0800

    remove redundant code + add options to MongoDB

--- app.js
+++ app.js
@@ -10,18 +10,17 @@ var recordsRouter = require('./routes/recordsRouter');
 
 var app = express();
 
-// users and records model set up;
-const Users = require('./models/users');
-const Records = require('./models/records');
 //connect MongoDB server and set up database connection
 const mongoose = require('mongoose');
+const options = { useNewUrlParser: true,  useUnifiedTopology: true};
 const url = "mongodb://localhost:27017/iTrailDB";
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, options);
 connect.then((db) => {
   console.log("Connected correctly to server!");
 }).catch((err) => {
   console.log(err);
 });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -46,7 +45,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
